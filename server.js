1d955b96fc8c5c1d38fefc735228e3ed0cc95936
francis <clyderitchie@yahoo.com>
2023-11-02 18:19:42 -0700

    Got the notes to save to the page

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // GET: /notes
 app.get('/notes', (req, res) => {
@@ -12,22 +14,40 @@ app.get('/notes', (req, res) => {
 
 // GET: /api/notes
 app.get('/api/notes', (req, res) => {
-   const notes = require('./db/db.json')
-   res.json(notes)
+    const notes = require('./db/db.json')
+    res.json(notes)
 })
 
 // POST: /api/notes
 app.post('/api/notes', (req, res) => {
-    const notes = require('./db/db.json');
-    notes.push(req.body)
-    fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), err =>{
-        if (err) {
-            console.log(err)
-        } else {
-            console.log('Note Saved!');
-            res.status(201).end();
-        }
-    })
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text
+        };
+        const recentNote = JSON.stringify(newNote);
+        const note = require('./db/db.json');
+        note.push(newNote);
+
+        fs.writeFile('./db/db.json', JSON.stringify(note, null, 2), err => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('Note was saved!');
+                res.status(201).end();
+            }
+        })
+
+        // const noteMade = {
+        //     newNote
+        //   };
+
+        //   res.status(201).json(noteMade);
+    } else {
+        console.log('Error adding note');
+        res.status(500).end();
+    }
 })
 
 // GET: Wildcard 
